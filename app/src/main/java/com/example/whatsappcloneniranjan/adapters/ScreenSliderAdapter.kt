b461a1a43e15e1694bc041fadd9d36ae46d81e54
Niranjan Nayak <niranjannayak0717@gmail.com>
2022-06-27 17:09:30 +0530

    1.1.2 ---> Adding Screen Slider Adapter of Fragments for using Chats and People Fragments

---
+++ app/src/main/java/com/example/whatsappcloneniranjan/adapters/ScreenSliderAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.whatsappcloneniranjan.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.whatsappcloneniranjan.fragments.ChatFragment
+import com.example.whatsappcloneniranjan.fragments.PeopleFragment
+
+class ScreenSliderAdapter(fa: FragmentActivity): FragmentStateAdapter(fa){
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment = when(position){
+        0 -> ChatFragment()
+        else -> PeopleFragment()
+    }
+}
