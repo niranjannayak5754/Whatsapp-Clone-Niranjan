22390d94c8a8b68b8952c7f63b5d8bdbb09c7d56
Niranjan Nayak <niranjannayak0717@gmail.com>
2022-06-27 17:11:07 +0530

    1.1.4 ---> Updating MainActivity for adapter and fragments

--- app/src/main/java/com/example/whatsappcloneniranjan/MainActivity.kt
+++ app/src/main/java/com/example/whatsappcloneniranjan/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.whatsappcloneniranjan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.whatsappcloneniranjan.adapters.ScreenSliderAdapter
 import com.example.whatsappcloneniranjan.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
 
+        binding.viewPager.adapter = ScreenSliderAdapter(this)
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "CHATS"
+                else -> tab.text = "PEOPLE"
+            }
+        }.attach()
     }
+
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.main_menu, menu)
+//
+//        return super.onCreateOptionsMenu(menu)
+//    }
 }
